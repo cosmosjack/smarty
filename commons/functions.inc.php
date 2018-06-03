167e2964ec9f1b3063f5bb4cc9ee2810e59cd14d
jack <997823131@qq.com>
2018-06-03 11:37:16 +0800

    666

--- commons/functions.inc.php
+++ commons/functions.inc.php
@@ -94,22 +94,22 @@ function get_pre_next($id=1,$class='wh_pre_next'){
     $str .= '<div class="'.$class.'">';
     $db = D('news');
     $data_pre = $db->where(array('news_id <'=>$id))->order("news_id desc")->find();
-    $data_next = $db->where(array('news_id <'=>$id))->order("news_id asc")->find();
+    $data_next = $db->where(array('news_id >'=>$id))->order("news_id asc")->find();
 
     if($data_pre){
         $pre_id = $data_pre['news_id'];
         $pre_desc = $data_pre['news_name'];
-        $str .= '<a href="'.SHOP_SITE_URL.'/whshow?id='.$pre_id.'">'.$pre_desc.'</a>';
+        $str .= '<span>上一篇:</span><a href="'.SHOP_SITE_URL.'/whshow?id='.$pre_id.'">'.$pre_desc.'</a>';
     }else{
-        $str .='<a href="javascript;">没有上一篇</a>';
+        $str .='<span>上一篇:</span><a href="javascript:;">没有上一篇</a>';
     }
 
     if($data_next){
         $next_id = $data_next['news_id'];
         $next_desc = $data_next['news_name'];
-        $str .= '<a href="'.SHOP_SITE_URL.'/whshow?id='.$next_id.'">'.$next_desc.'</a>';
+        $str .= '<span>下一篇:</span><a href="'.SHOP_SITE_URL.'/whshow?id='.$next_id.'">'.$next_desc.'</a>';
     }else{
-        $str .='<a href="javascript;">没有下一篇</a>';
+        $str .='<span>下一篇:</span><a href="javascript;">没有下一篇</a>';
     }
 
     $str .='</div>';

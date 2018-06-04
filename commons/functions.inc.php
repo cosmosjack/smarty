bc4ca60093ba1ffbad860926764b9aef0705a42e
jack <997823131@qq.com>
2018-06-04 11:14:07 +0800

    666

--- commons/functions.inc.php
+++ commons/functions.inc.php
@@ -134,3 +134,23 @@ function whshow_position($id=1,$class='whshow_position'){
     return $str;
 }
 /* 获取详情页的定位 面包屑 end */
+
+// 根据经纬度获取 之间的距离 start
+function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+    $theta = $lon1 - $lon2;
+    $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+    $dist = acos($dist);
+    $dist = rad2deg($dist);
+    $miles = $dist * 60 * 1.1515;
+    $unit = strtoupper($unit);
+
+    if ($unit == "K") {
+        return ($miles * 1.609344);
+    } else if ($unit == "N") {
+        return ($miles * 0.8684);
+    } else {
+        return $miles;
+    }
+}
+// 根据经纬度获取 之间的距离 end
+

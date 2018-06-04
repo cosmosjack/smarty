57f625cec0c05aa341a02e8bd04e60b4e1681421
jack <997823131@qq.com>
2018-06-04 17:46:21 +0800

    666

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     define(DS,DIRECTORY_SEPARATOR);
     define("SHOP_SITE_URL","http://".$_SERVER['HTTP_HOST']);
 //	define("TPLSTYLE", "fst");                        //默认模板存放的目录
-    define('PC_TPL',"fst");
-    define('MOBILE_TPL',"fst_mobile");
+    define('PC_TPL',"tms");
+//    define('MOBILE_TPL',"fst_mobile");
 	define("APP", "home");           //设置当前应用的目录
 	require('./brophp/bro.php'); //加载框架的入口文件
 

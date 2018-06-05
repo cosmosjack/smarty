50347fd785d3bfa1ad08211c3035fc9962b3c661
jack <997823131@qq.com>
2018-06-05 23:35:42 +0800

    20180605

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	/**
 	 * 单一入口文件
 	 */
-    define(DS,DIRECTORY_SEPARATOR);
+    define('DS',DIRECTORY_SEPARATOR);
     define("SHOP_SITE_URL","http://".$_SERVER['HTTP_HOST']);
 //	define("TPLSTYLE", "fst");                        //默认模板存放的目录
     define('PC_TPL',"tms");

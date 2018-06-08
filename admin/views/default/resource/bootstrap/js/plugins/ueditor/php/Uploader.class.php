c574bd63cb53d85edad79b2ff752b42008a1a347
jack <997823131@qq.com>
2018-06-08 17:00:02 +0800

    666

--- admin/views/default/resource/bootstrap/js/plugins/ueditor/php/Uploader.class.php
+++ admin/views/default/resource/bootstrap/js/plugins/ueditor/php/Uploader.class.php
@@ -358,10 +358,15 @@ class Uploader
      * 获取当前上传成功文件的各项信息
      * @return array
      */
-    public function getFileInfo($is_http=false)
+    public function getFileInfo($is_pre=false,$is_https=false)
     {
-        if($is_http){
-            $this->fullName = 'http://'.$_SERVER['SERVER_NAME'].$this->fullName;
+        if($is_https){
+            $http = 'https://';
+        }else{
+            $http = 'http://';
+        }
+        if($is_pre){
+            $this->fullName = $http.$_SERVER['SERVER_NAME'].$this->fullName;
         }
         return array(
             "state" => $this->stateInfo,

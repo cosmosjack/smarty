f1ba3d7fda95ccf40e2dc155815def0114d0f1f6
jack <997823131@qq.com>
2018-06-08 15:59:55 +0800

    666

--- admin.php
+++ admin.php
@@ -3,8 +3,7 @@
 	 * 单一入口文件
 	 */
 
-    define(DS,DIRECTORY_SEPARATOR);
-    define("SHOP_SITE_URL","http://".$_SERVER['HTTP_HOST']);
+    define('DS',DIRECTORY_SEPARATOR);
     define("BAIDU_API", "http://data.zz.baidu.com/urls?site=www.hzjftsp.com&token=qHlho16rlfwHhPXJ");//
 	define("TPLSTYLE", "default");                        //默认模板存放的目录
 	define("APP", "admin");           //设置当前应用的目录

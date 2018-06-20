490277c81c21d2e52b625d69259b991142f25cb9
jack <997823131@qq.com>
2018-06-20 15:54:48 +0800

    666

--- home/controls/whshow.class.php
+++ home/controls/whshow.class.php
@@ -9,31 +9,14 @@
  */
 class whshow {
     public function index(){
-        $db_news = D("news");
-
-        $default_column = $this->get_default_column();
-//            p($default_column);
-        $this->assign("data_column",$default_column);
-
-        //左侧数据 start
-        $db_news_cls = D('news_cls');
-        $data_news_cls = $db_news_cls->where(array('cls_pid'=>29))->select();
-        foreach($data_news_cls as $k=>$v){
-            $tmp_data = $db_news->field("news_id,news_name,news_cls_id")->where(array('news_cls_id'=>$v['news_cls_id']))->order("add_times desc")->limit(3)->select();
-            $data_news_cls[$k]['news_data'] = $tmp_data;
-        }
-        $json_data['left_data'] = $data_news_cls;
-        //左侧数据 end
-
-        $data_news = $db_news->where(array('news_id'=>$_GET['id']))->find();
-        $data_news['news_body'] = htmlspecialchars_decode($data_news['news_body']);
-        $json_data['data_news'] = $data_news;
-        $this->assign("json_data",$json_data);
 
         $this->display();
     }
 
     function mobile_index(){
         $this->display('index');
+    }
+    function play(){
+        $this->display();
     }
 }

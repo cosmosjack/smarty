d6c2b14fe8ddff552e9809172a0a206e1ea08a91
yangqilin <928944169@qq.com>
2018-08-09 17:06:42 +0800

    管理员列表，添加，删除，修改密码，修改了admin_user表字段

--- admin/controls/admin.class.php
+++ admin/controls/admin.class.php
@@ -13,13 +13,67 @@ class admin extends Action{
     }
     // 添加管理员
     function admin_add(){
-
-        $this->display();
+        if(empty($_POST['user_name']) || empty($_POST['user_passwd']))
+            $this->error('参数错误');
+        $db_admin_user = D('admin_user');
+        $info = $db_admin_user->where(array('user_name'=>$_POST['user_name']))->find();
+        if(!empty($info))
+            $this->error('用户名已存在');
+        $insert = array();
+        $insert['user_name'] = trim($_POST['user_name']);
+        $insert['user_passwd'] = md5(trim($_POST['user_passwd']));
+        $insert['last_login_time'] = time();
+        $insert['add_time'] = time();
+        if($db_admin_user->insert($insert))
+            $this->success('添加成功');
+        else
+            $this->error('添加失败');
     }
     // 管理员列表
     function admin_list(){
-
+        $db_admin_user = D('admin_user');
+        $user_list = $db_admin_user->select();
+        $this->assign('user_list',$user_list);
         $this->display();
+    }
+    //修改管理员
+    function admin_eidt(){
+        $user_id = intval($_POST['user_id']);
+        if(empty($user_id) || empty($_POST['user_passwd_new']))
+            $this->error('参数错误');
+        $db_admin_user = D('admin_user');
+        $info = $db_admin_user->where($user_id)->find();
+        if(empty($info))
+            $this->error('管理员不存在');
+        //只允许修改自己的密码
+        if($info['user_name'] != $_SESSION['admin_user']['user_name'])
+            $this->error('不能修改其他人的密码');
+        if($info['user_passwd'] != md5($_POST['user_passwd']))
+            $this->error('密码不正确');
+        $update = array();
+        $update['user_passwd'] = md5($_POST['user_passwd_new']);
+        if($db_admin_user->where($user_id)->update($update))
+            $this->success('修改成功');
+        else
+            $this->error('无任何修改');
+    }
+    //删除管理员
+    function del_admin(){
+        if($_SESSION['admin_user']['user_name'] != 'admin')
+            ajaxReturn(array('control'=>'del_admin','code'=>0,'msg'=>'超级管理员才能删除'));
+        $user_id = intval($_POST['user_id']);
+        if(empty($user_id))
+            ajaxReturn(array('control'=>'del_admin','code'=>0,'msg'=>'参数错误'));
+        $db_admin_user = D('admin_user');
+        $info = $db_admin_user->where($user_id)->find();
+        if(empty($info))
+            ajaxReturn(array('control'=>'del_admin','code'=>0,'msg'=>'管理员不存在'));
+        if($info['user_name'] == 'admin')
+            ajaxReturn(array('control'=>'del_admin','code'=>0,'msg'=>'超级管理员不能删除'));
+        if($db_admin_user->where($user_id)->delete())
+            ajaxReturn(array('control'=>'del_admin','code'=>200,'msg'=>'删除成功'));
+        else
+            ajaxReturn(array('control'=>'del_admin','code'=>0,'msg'=>'删除失败'));
     }
     //管理员登录
     function login(){
@@ -34,8 +88,7 @@ class admin extends Action{
                         ->where(array('user_name'=>$_POST['user']),
                             array('email'=>$_POST['user']),
                             array('user_phone'=>$_POST['user'])
-                        )
-                        ->find();
+                        )->find();
                     if($data_agent_user){
                         /* 判断是否是机器登录 start */
                         $db_agent_login = new Dpdo();
@@ -62,6 +115,11 @@ class admin extends Action{
                             $update['good_time'] = time();
                             $update['num'] = 0;
                             $db_agent_login->where(array('user_id'=>$data_agent_user['user_id']))->update($update);
+                            //修改登录时间和登录次数
+                            $update = array();
+                            $update['times'] = $data_agent_user['times']+1;
+                            $update['last_login_time'] = time();
+                            $db_agent_user->where($data_agent_user['user_id'])->update($update);
 //                            ajaxReturn(array('control'=>'login_in','state'=>200,'msg'=>'登录成功'));
 
                             $this->redirect("index/index");
@@ -93,7 +151,7 @@ class admin extends Action{
     function logout(){ //退出
         $_SESSION['is_login'] = false;
         $_SESSION['admin_user'] = '';
-        echo '<script>alert("退出成功");</script>';
+        $this->success('退出成功','admin/login');
         $this->redirect('admin/login');
     }
 }
